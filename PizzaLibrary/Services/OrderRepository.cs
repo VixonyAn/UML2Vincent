fe924b0fdafe88b747eb1be78017de3cc6bf5548
VixonyAn <vixonyan@gmail.com>
2024-12-05 12:18:49 +0100

    small edits to orders

--- PizzaLibrary/Services/OrderRepository.cs
+++ PizzaLibrary/Services/OrderRepository.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PizzaLibrary.Models;
+using PizzaLibrary.Interfaces;
 
 namespace PizzaLibrary.Services
 {
-    public class OrderRepository
+    public class OrderRepository : IOrderRepository
     {
         private List<Order> _orderList;
+
+        public int Count { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AddOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
